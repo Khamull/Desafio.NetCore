c1d852f08e404cfdd3670643f4956c251b8ea7db
Charles Path <charles.path@outlook.com>
2021-01-15 16:23:08 -0300

    First Commit
    Done after creating the first steps of the APIS!
    Considering finalizing the work withtou the front end!

---
+++ Amount.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Desafio
+{
+    public class Amount
+    {
+        private decimal InitialValue;
+        private int Time;
+        private decimal IR;
+
+        public decimal _IR
+        {
+            get { return IR; }
+            set { IR = value; }
+        }
+
+        public int _Time
+        {
+            get { return Time; }
+            set { Time = value; }
+        }
+
+
+        public decimal _InitialValue
+        {
+            get { return InitialValue; }
+            set { InitialValue = value; }
+        }
+
+        private decimal FinalValue()
+        {
+            var tax = (decimal)Math.Pow((double)(1 + IR), (double)_Time);
+            var finalValue = _InitialValue * tax;
+            return finalValue;
+        }
+
+        public decimal GetFinalValue()
+        {
+            return FinalValue();
+        }
+    }
+}
---
+++ InterestRate.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Desafio
+{
+    public class InterestRate
+    {
+        private static double IRate => 0.01;
+
+        public static double _Irate
+        {
+            get { return IRate; }
+        }
+
+        static public double GetIRate() => _Irate;
+    }
+}
