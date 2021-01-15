2121afcf44f911b1ffcb335de1915d16b6d993b3
Charles Path <charles_path7@hotmail.com>
2021-01-15 16:33:56 -0300

    Missing Files
    When I did the initial upload, some files where missing!

---
+++ Controllers/TaxaJuros.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Desafio.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TaxaJuros : ControllerBase
+    {
+        private readonly ILogger<TaxaJuros> _logger;
+
+        public TaxaJuros(ILogger<TaxaJuros> logger)
+        {
+            _logger = logger;
+        }
+        // GET: TaxaJuros
+        [HttpGet]
+        public ActionResult<InterestRate> Get()
+        {
+            return Ok(InterestRate.GetIRate());
+        }
+    }
+}
